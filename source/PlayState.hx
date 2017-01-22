88e5b13019c0008c4713029eacd7a2ac0acdd2da
Grégoire Liglet <gregoire.liglet@epitech.eu>
2017-01-22 11:28:35 -0800

    initial commit :sparkles:

---
+++ source/PlayState.hx
@@ -0,0 +1,30 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.math.FlxMath;
+
+// import flixel.addons.display.FlxBackdrop;
+
+class PlayState extends FlxState
+{
+	// private var _background:FlxBackdrop;
+	private var _player:Player;
+	
+	override public function create():Void
+	{
+		// _background = new FlxBackdrop();
+		_player = new Player(20, 20);
+		add(_player);
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+	
+}
